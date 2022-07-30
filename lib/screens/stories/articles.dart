64c14ab0945e79c406d02a78137f93d2533ee2a0
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-07-30 19:21:09 +0100

    iono

--- lib/screens/stories/articles.dart
+++ lib/screens/stories/articles.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../sizeConfig.dart';
+
 class Articles extends StatefulWidget {
   const Articles({Key? key}) : super(key: key);
   @override
@@ -11,16 +13,44 @@ class Articles extends StatefulWidget {
 class _ArticlesState extends State<Articles> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig.init(context);
     return Scaffold(
-      backgroundColor: Colors.black12,
-      body: Center(
-        child: Text(
-          'Articles',
-          style: GoogleFonts.lato(
-            textStyle: TextStyle(color: Colors.white12, letterSpacing: .5),
-          ),
-        ),
-      ),
-    );
+        backgroundColor: Colors.black12,
+        body: Container(
+            padding: EdgeInsets.all(15),
+            // child: SingleChildScrollView(
+            child: SingleChildScrollView(
+                child: Column(children: [
+              SizedBox(height: getProportionateScreenHeight(80)),
+              Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 50.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Articles',
+                        style: GoogleFonts.hammersmithOne(
+                          textStyle: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: .5,
+                            fontWeight: FontWeight.bold,
+                            fontSize: getProportionateScreenWidth(25),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: getProportionateScreenHeight(5)),
+              CupertinoSearchTextField(
+                placeholder: "Search",
+                onSubmitted: (String value) {
+                  print(value);
+                },
+              ),
+              SizedBox(height: getProportionateScreenHeight(5)),
+            ]))));
   }
 }
